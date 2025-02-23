99a471f6c79cd0b435ccaebc47316e66ca2def04
Rohan Kapadia <Rohan.Kapadia@coles.com.au>
2025-02-23 19:02:11 +1100

    add validation for the config

--- presidioredactionprocessor/internal/presidioclient/config.go
+++ presidioredactionprocessor/internal/presidioclient/config.go
@@ -4,7 +4,8 @@
 package presidioclient
 
 import (
-	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
 	"go.opentelemetry.io/collector/component"
@@ -59,13 +60,88 @@ type EntityAnonymizer struct {
 	Key         string `mapstructure:"key,omitempty"`
 }
 
-func (c *PresidioRedactionProcessorConfig) validate() error {
-	if c.PresidioRunMode == "service" {
-		if c.PresidioServiceConfig.AnalyzerEndpoint == "" {
-			return errors.New("presidio_service.analyzer_endpoint is required when mode is external")
+func (c *PresidioRedactionProcessorConfig) Validate() error {
+	if c.PresidioRunMode != "embedded" && c.PresidioRunMode != "external" {
+		return fmt.Errorf("invalid presidio_run_mode: %s, must be either 'embedded' or 'external'", c.PresidioRunMode)
+	}
+
+	if c.PresidioRunMode == "external" {
+		if err := c.validateExternalServiceConfig(); err != nil {
+			return err
 		}
-		if c.PresidioServiceConfig.AnonymizerEndpoint == "" {
-			return errors.New("presidio_service.anonymizer_endpoint is required when mode is external")
+	}
+
+	for i, anonymizer := range c.AnonymizerConfig.Anonymizers {
+		if err := validateEntityAnonymizer(anonymizer, i); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *PresidioRedactionProcessorConfig) validateExternalServiceConfig() error {
+	if c.PresidioServiceConfig.AnalyzerEndpoint == "" {
+		return fmt.Errorf("presidio_service.analyzer_endpoint is required when mode is external")
+	}
+
+	if !strings.HasPrefix(c.PresidioServiceConfig.AnalyzerEndpoint, "http://") &&
+		!strings.HasPrefix(c.PresidioServiceConfig.AnalyzerEndpoint, "https://") {
+		return fmt.Errorf("invalid analyzer_endpoint protocol: %s, must start with http:// or https://",
+			c.PresidioServiceConfig.AnalyzerEndpoint)
+	}
+
+	if c.PresidioServiceConfig.AnonymizerEndpoint == "" {
+		return fmt.Errorf("presidio_service.anonymizer_endpoint is required when mode is service")
+	}
+	if !strings.HasPrefix(c.PresidioServiceConfig.AnonymizerEndpoint, "http://") &&
+		!strings.HasPrefix(c.PresidioServiceConfig.AnonymizerEndpoint, "https://") {
+		return fmt.Errorf("invalid anonymizer_endpoint protocol: %s, must start with http:// or https://",
+			c.PresidioServiceConfig.AnonymizerEndpoint)
+	}
+	return nil
+}
+
+func validateEntityAnonymizer(anonymizer EntityAnonymizer, index int) error {
+	if anonymizer.Entity == "" {
+		return fmt.Errorf("anonymizer[%d].entity is required", index)
+	}
+
+	validTypes := map[string]bool{
+		"REPLACE": true,
+		"HASH":    true,
+		"REDACT":  true,
+		"MASK":    true,
+		"ENCRYPT": true,
+	}
+
+	if !validTypes[anonymizer.Type] {
+		return fmt.Errorf("anonymizer[%d].type has invalid value: %s", index, anonymizer.Type)
+	}
+
+	switch anonymizer.Type {
+	case "REPLACE":
+		if anonymizer.NewValue == "" {
+			return fmt.Errorf("anonymizer[%d].new_value is required for REPLACE type", index)
+		}
+	case "HASH":
+		if anonymizer.HashType == "" {
+			return fmt.Errorf("anonymizer[%d].hash_type is required for HASH type", index)
+		}
+		validHashTypes := map[string]bool{"sha256": true, "sha512": true, "md5": true}
+		if !validHashTypes[anonymizer.HashType] {
+			return fmt.Errorf("anonymizer[%d].hash_type has invalid value: %s", index, anonymizer.HashType)
+		}
+	case "MASK":
+		if anonymizer.CharsToMask <= 0 {
+			return fmt.Errorf("anonymizer[%d].chars_to_mask must be greater than 0 for MASK type", index)
+		}
+		if anonymizer.MaskingChar == "" {
+			anonymizer.MaskingChar = "*"
+		}
+	case "ENCRYPT":
+		if anonymizer.Key == "" {
+			return fmt.Errorf("anonymizer[%d].key is required for ENCRYPT type", index)
 		}
 	}
 
